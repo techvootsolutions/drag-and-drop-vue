010364e44a6df9653f13445b350efa609fa44cfc
tvshreyanshi <shreyanshi@techvoot.com>
2024-01-09 11:59:36 +0530

    remove code

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
