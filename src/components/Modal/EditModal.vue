e2baf57f1541a027c06997a8923b6363b0035a5c
tvshreyanshi <shreyanshi@techvoot.com>
2024-01-22 15:17:16 +0530

    formating code

--- src/components/Modal/EditModal.vue
+++ src/components/Modal/EditModal.vue
@@ -4,116 +4,43 @@
       <div class="tvd__modal-container" ref="target">
         <div class="tvd__modal-header">
           <h6>{{ isEdit ? "Edit Card" : "Show Card" }}</h6>
-          <button
-            class="tvd__close_editmodal tvd__modal-close"
-            @click="$emit('modal-close')"
-          >
-            <i class="tvd__icons fa-solid fa-xmark"></i>
-          </button>
+          <button class="tvd__close_editmodal tvd__modal-close" @click="$emit('modal-close')"><i class="tvd__icons fa-solid fa-xmark"></i></button>
         </div>
         <hr class="tvd__hr" />
         <div class="tvd__modal-body">
           <div v-if="!isEdit">
-            <div
-              class="tvd__show_card"
-              v-if="getData.event && getData.event.title"
-            >
-              <h6>{{ getData.event.title }}</h6>
-            </div>
-            <div
-              class="tvd__show_card"
-              v-if="getData.event && getData.event.deadlineDate"
-            >
-              <span><b>DeadLine: </b></span>
-              <span>{{ getData.event.deadlineDate }}</span>
-            </div>
-            <div
-              class="tvd__show_card"
-              v-if="getData.event && getData.event.description"
-            >
-              <span v-html="getData.event.description"></span>
-            </div>
-            <div
-              class="tvd__image"
-              v-if="getData.event && getData.event.attachment"
-            >
-              <!-- <div v-for="(data, index) in getData.event.attachment" :key="index">
-                <div v-if="data.type && data.type.includes('image')">
-                  <img class="tvd__img_attachment" :src="data.dataUrl" alt="image" />
-                </div>
-              </div> -->
-              <div
-                v-for="(attachmentItem, index) in getData.event.attachment"
-                :key="index"
-              >
+            <div class="tvd__show_card" v-if="getData.event && getData.event.title"><h6>{{ getData.event.title }}</h6></div>
+            <div class="tvd__show_card" v-if="getData.event && getData.event.deadlineDate"><span><b>DeadLine: </b></span><span>{{ getData.event.deadlineDate }}</span></div>
+            <div class="tvd__show_card" v-if="getData.event && getData.event.description"><span v-html="getData.event.description"></span></div>
+            <div class="tvd__image" v-if="getData.event && getData.event.attachment">
+              <div v-for="(attachmentItem, index) in getData.event.attachment" :key="index">
                 <div v-if="attachmentItem.type.includes('image')">
-                <a :href="attachmentItem.url" target="_blank">
-                  <img
-                    :src="attachmentItem.url"
-                    :alt="`Image Attachment ${index + 1}`"
-                    style="max-width: 100%"
-                    class="tvd__img_attachment"
-                  />
-                </a>
+                  <a :href="attachmentItem.url" target="_blank">
+                    <img :src="attachmentItem.url" :alt="`Image Attachment ${index + 1}`" style="max-width: 100%" class="tvd__img_attachment" />
+                  </a>
                 </div>
                 <div v-else-if="attachmentItem.type.includes('video')">
                   <a href="#" @click="openVideoInNewTab(attachmentItem.url)">
-                <video
-                  controls
-                  :src="attachmentItem.url"
-                  style="max-width: 100%"
-                  class="tvd__img_attachment"
-                ></video></a>
+                    <video controls :src="attachmentItem.url" style="max-width: 100%" class="tvd__img_attachment"></video>
+                  </a>
                 </div>
                 <div v-else-if="attachmentItem.type.includes('pdf')">
-                <!-- You can use a PDF viewer library here -->
-                <p>PDF Attachment</p>
-                <div v-html="attachmentItem.htmlElement"></div>
-                <!-- </div> -->
-                <!-- <div v-else> -->
-                <!-- Handle other file types as needed -->
-                <p>{{ attachmentItem.type }}: Not supported for preview</p>
+                  <p>PDF Attachment</p>
+                  <div v-html="attachmentItem.htmlElement"></div>
+                  <p>{{ attachmentItem.type }}: Not supported for preview</p>
                 </div>
               </div>
             </div>
           </div>
           <div v-if="isEdit">
             <form @submit.prevent="UpdateData()">
-              <div class="tvd__form-group">
-                <label class="tvd__form-label">Title</label>
-                <input class="tvd__form-control" type="text" v-model="title" />
-              </div>
-              <div class="tvd__form-group">
-                <label class="tvd__form-label">Description</label>
-                <textarea
-                  class="tvd__form-control"
-                  rows="4"
-                  v-model="description"
-                ></textarea>
-              </div>
-              <div class="tvd__form-group">
-                <label class="tvd__form-label">Due Date</label>
-                <input
-                  class="tvd__form-control"
-                  type="date"
-                  v-model="duedate"
-                />
-              </div>
-              <div class="tvd__form-group">
-                <label class="tvd__form-label">Attachment</label>
-                <input
-                  class="tvd__form-control"
-                  type="file"
-                  multiple
-                  @change="handleFileChange"
-                />
-              </div>
-
+              <div class="tvd__form-group"><label class="tvd__form-label">Title</label><input class="tvd__form-control" type="text" v-model="title" /></div>
+              <div class="tvd__form-group"><label class="tvd__form-label">Description</label><textarea class="tvd__form-control" rows="4" v-model="description"></textarea></div>
+              <div class="tvd__form-group"><label class="tvd__form-label">Due Date</label><input class="tvd__form-control" type="date" v-model="duedate" /></div>
+              <div class="tvd__form-group"><label class="tvd__form-label">Attachment</label><input class="tvd__form-control" type="file" multiple @change="handleFileChange" /></div>
               <div class="tvd__modal-footer">
                 <button type="file" class="tvd__edit_btn">Submit</button>
-                <button class="tvd__edit_btn" @click="$emit('modal-close')">
-                  Close
-                </button>
+                <button class="tvd__edit_btn" @click="$emit('modal-close')">Close</button>
               </div>
             </form>
           </div>
@@ -127,52 +54,25 @@
 <script setup>
 import { watch, defineProps, defineEmits, ref, computed } from "vue";
 import { onClickOutside } from "@vueuse/core";
-
-const props = defineProps({
-  isOpen: {
-    type: Boolean,
-    default: false,
-  },
-  isEdit: {
-    type: Boolean,
-    default: false,
-  },
-  data: {
-    default: () => ({}),
-  },
-  type: String,
-});
-const title = ref("");
-const description = ref("");
-const duedate = ref(null);
-const attachment = ref([]);
-const emit = defineEmits(["modal-close"], ["edit-data"]);
-
-const target = ref(null);
-
+const props = defineProps({isOpen:{type:Boolean,default:false},isEdit:{type:Boolean,default:false},data:{default:()=>({})},type:String});
+const title = ref("");const description = ref("");const duedate = ref(null);const attachment = ref([]);const emit = defineEmits(["modal-close"],["edit-data"]);const target = ref(null);
 const handleFileChange = (event) => {
   const selectedFiles = event.target.files;
-
   if (selectedFiles && selectedFiles.length > 0) {
     const newAttachments = [];
-
     for (const file of selectedFiles) {
       const type = file.type;
       const reader = new FileReader();
-
       if (isFileTypeSupported(type)) {
-        console.log('in if in type');
         reader.onload = () => {
           newAttachments.push({
             type,
             file,
             url: reader.result,
           });
-
           if (newAttachments.length === selectedFiles.length) {
             attachment.value = newAttachments;
           }
-          console.log('attachment', attachment.value);
         };
       } else {
         console.warn(`Unsupported file type: ${type}`);
@@ -182,11 +82,7 @@ const handleFileChange = (event) => {
     console.error("Selected files are not an array or empty:", selectedFiles);
   }
 };
-
-const openVideoInNewTab = (linkUrl) => {
-  window.open(linkUrl, '_blank');
-}
-
+const openVideoInNewTab = (linkUrl) => {window.open(linkUrl, "_blank");};
 const isFileTypeSupported = (type) => {
   const supportedTypes = ["image", "video", "pdf"];
   return supportedTypes.some((supportedType) => type.startsWith(supportedType));
@@ -196,44 +92,26 @@ onClickOutside(target, () => {
     emit("modal-close");
   }
 });
-
-// const convertHeicToJpeg = async(heicFile, reader) => {
-//   try {
-//         const dataUrl = await heif2any({
-//           blob: heifFile,
-//           toType: 'image/jpeg',
-//           quality: 0.7,
-//         });
-//         reader.result = dataUrl;
-//         reader.onload(); // Trigger the onload event manually
-//       } catch (error) {
-//         console.error('Error converting HEIF to JPEG:', error);
-//       }
-// }
-const getData = computed(() => {
-  return props.data;
-});
+const getData = computed(() => {return props.data;});
 const UpdateData = () => {
   const editData = {
     title: title.value,
     description: description.value,
     deadlineDate: duedate.value ? duedate.value : new Date().toISOString(),
-    attachment: attachment.value ? getData.value.event.attachment.concat(attachment.value) : [],
+    attachment: attachment.value
+      ? getData.value.event.attachment.concat(attachment.value)
+      : [],
     arrayindex: getData.value.arrayindex,
     itemindex: getData.value.itemindex,
   };
-  console.log('editData after concate', editData);
   emit("edit-data", editData);
 };
 watch(getData, async (val) => {
   if (val) {
-    console.log("val", val);
     title.value = val.event.title;
     description.value = val.event.description;
     duedate.value = val.event.deadlineDate;
     attachment.value = val.event.attachment;
   }
-  console.log('val', val);
 });
 </script>
-
--- src/components/SimpleDrag.vue
+++ src/components/SimpleDrag.vue
@@ -1,81 +1,41 @@
 <template>
   <section v-if="response">
     <ul class="tvd__column__list">
-      <li
-        class="tvd__column__item"
-        v-for="(data, index) in response"
-        :key="index"
-      >
-        <!-- section title -->
+      <li class="tvd__column__item" v-for="(data, index) in response" :key="index">
         <div class="tvd__column__title--wrapper">
-          <h2 v-if="editSectionTitle !== index && data.title">
-            {{ data.title }}
-          </h2>
+          <h2 v-if="editSectionTitle !== index && data.title">{{ data.title }}</h2>
           <div v-if="editSectionTitle === index" class="tvd__section-title">
-            <input
-              v-model="data.title"
-              type="text"
-              placeholder="Section Title"
-              class="tvd__add-input"
-              />
-              <button class="tvd__edit_card" @click="saveEdit(index, data)">
-                <i class="tvd__icons fa-solid fa-check"></i>
-              </button>
-              <button class="tvd__edit_card" @click="closeEditSection()">
-                <i class="tvd__icons fa-solid fa-xmark"></i>
-              </button>
+            <input v-model="data.title" type="text" placeholder="Section Title" class="tvd__add-input"/>
+            <button class="tvd__edit_card" @click="saveEdit(index, data)">
+              <i class="tvd__icons fa-solid fa-check"></i>
+            </button>
+            <button class="tvd__edit_card" @click="closeEditSection()">
+              <i class="tvd__icons fa-solid fa-xmark"></i>
+            </button>
           </div>
-          <div v-if="editSectionTitle === index">
-          </div>
-          <button
-            v-else
-            class="tvd__edit_card"
-            @click="showEditCardUI(index, data)"
-          >
+          <div v-if="editSectionTitle === index"></div>
+          <button v-else class="tvd__edit_card" @click="showEditCardUI(index, data)">
             <i class="fa-solid fa-pen-to-square tvd__icons"></i>
           </button>
           <div>
-            <i
-              class="tvd__column__item--cta fa-solid fa-plus tvd__cursor-pointer"
-              @click.prevent="showAddCardUI(index)"
-            ></i>
-            <!-- <i class="tvd__icons fa-solid fa-ellipsis"></i> -->
+            <i class="tvd__column__item--cta fa-solid fa-plus tvd__cursor-pointer" @click.prevent="showAddCardUI(index)"></i>
           </div>
         </div>
-        <ul
-          class="tvd__card__list"
-          v-if="data.data"
-          @drop.prevent="dropItem(index, $event)"
-          @dragover.prevent="allowDrop"
-        >
+        <ul class="tvd__card__list" v-if="data.data" @drop.prevent="dropItem(index, $event)" @dragover.prevent="allowDrop">
           <template v-for="(item, itemindex) in data.data" :key="itemindex">
-            <li
-              :class="['tvd__card__item', { 'card-dragged': isDragging && draggedIndex === index }]"
-              v-if="Object.keys(item).length > 0 && !$slots.customCard"
-              :draggable="true"
-              @dragstart="dragStart(index, itemindex)"
-              @dragover.prevent="internalAllowDrop"
-              @drop.prevent="internalDrop(index, itemindex)"
-            >
+            <li :class="['tvd__card__item', { 'card-dragged': isDragging && draggedIndex === index }]"
+                v-if="Object.keys(item).length > 0 && !$slots.customCard"
+                :draggable="true"
+                @dragstart="dragStart(index, itemindex)"
+                @dragover.prevent="internalAllowDrop"
+                @drop.prevent="internalDrop(index, itemindex)">
               <div class="tvd__card__field">
-                <!-- card title -->
-                <span
-                  v-if="item && item.title"
-                  v-html="item.title"
-                  class="tvd__card__tag"
-                  @click="openModal(item, index, itemindex, false)"
-                ></span>
+                <span v-if="item && item.title" v-html="item.title" class="tvd__card__tag" @click="openModal(item, index, itemindex, false)"></span>
                 <div class="tvd__card-button-wrapper">
-                  <button
-                    @click="openModal(item, index, itemindex, true)"
-                    class="tvd__edit_card"
-                  >
+                  <button @click="openModal(item, index, itemindex, true)" class="tvd__edit_card">
                     <i class="fa-solid fa-pen-to-square tvd__icons"></i>
                   </button>
-                  <button
-                    @click="openDeleteModal(index, itemindex)"
-                    class="tvd__delete_card"
-                  >
+                  <button @click="openDeleteModal(index, itemindex)" class="tvd__delete_card">
                     <i class="tvd__icons fa-solid fa-trash-can"></i>
                   </button>
                 </div>
@@ -83,34 +43,19 @@
               <div class="tvd__card__field" v-if="item && item.deadlineDate">
                 <span>{{ item.deadlineDate }}</span>
               </div>
-              <!-- card description -->
               <div class="tvd__card__field" v-if="item && item.description">
                 <span class="tvd__description" v-html="item.description"></span>
               </div>
               <div class="tvd__card__actions">
                 <li class="tvd__card__actions--wrapper">
-                  <!-- <i class="icons fa-solid fa-align-left"></i>
-                <i class="icons fa-regular fa-comment"></i> -->
                   <div class="tvd__card__avatars">
-                    <li class="tvd__card__avatars--item">
-                      <!-- <img
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuXwdzjy8m5Awd5zZ6GknOv2dVo0xYeHEvJu3JGU3SlXEH7y1WV4GIpgSlAmQIN4a0qBI&usqp=CAU"
-                      alt="Man standing near balcony"
-                      class="tvd__avatar__image"
-                    /> -->
-                    </li>
+                    <li class="tvd__card__avatars--item"></li>
                   </div>
                 </li>
               </div>
             </li>
-            <li
-              class="tvd__card__item"
-              v-if="$slots.customCard"
-              :draggable="true"
-              @dragstart="dragStart(index, itemindex)"
-              @dragover.prevent="internalAllowDrop"
-              @drop.prevent="internalDrop(index, itemindex)"
-            >
+            <li class="tvd__card__item" v-if="$slots.customCard" :draggable="true" @dragstart="dragStart(index, itemindex)"
+                @dragover.prevent="internalAllowDrop" @drop.prevent="internalDrop(index, itemindex)">
               <slot :item="item" name="customCard"></slot>
             </li>
           </template>
@@ -120,17 +65,8 @@
             </button>
             <slot name="cardForm" />
             <div v-if="!$slots.cardForm">
-              <input
-                v-model="newCard.title"
-                type="text"
-                placeholder="Card Title"
-                class="tvd__add-input"
-              />
-              <textarea
-                v-model="newCard.description"
-                placeholder="Card Description"
-                class="tvd__add-textarea"
-              ></textarea>
+              <input v-model="newCard.title" type="text" placeholder="Card Title" class="tvd__add-input"/>
+              <textarea v-model="newCard.description" placeholder="Card Description" class="tvd__add-textarea"></textarea>
               <button class="tvd__add-btn" @click.prevent="addCard(index)">
                 <h6>{{ addCardTitle }}</h6>
               </button>
@@ -147,12 +83,7 @@
           <button class="tvd__close_section" @click="closeNewList()">
             <i class="tvd__icons fa-solid fa-xmark"></i>
           </button>
-          <input
-            v-model="newList.listTitle"
-            type="text"
-            placeholder="Section Title"
-            class="tvd__add-input"
-          />
+          <input v-model="newList.listTitle" type="text" placeholder="Section Title" class="tvd__add-input"/>
           <button class="tvd__add-btn" @click.prevent="addList()">
             <h6>{{ addSectionTitle }}</h6>
           </button>
@@ -164,21 +95,9 @@
         </button>
       </li>
     </ul>
-    <EditModal
-      :isEdit="isEdit"
-      :isOpen="isModalOpened"
-      @modal-close="closeModal"
-      @edit-data="editdata"
-      name="first-modal"
-      :data="geteditData"
-    />
-    <DeleteConfirmation
-      deleteMessage="Are you sure you want to Delete the Card?"
-      :isOpenDeleteModal="isOpenDeleteModal"
-      @modal-close="closeDeleteModal"
-      @confirm-delete="deleteCard()"
-      name="delete-confirmation-modal"
-    />
+    <EditModal :isEdit="isEdit" :isOpen="isModalOpened" @modal-close="closeModal" @edit-data="editdata" name="first-modal" :data="geteditData" />
+    <DeleteConfirmation deleteMessage="Are you sure you want to Delete the Card?" :isOpenDeleteModal="isOpenDeleteModal" @modal-close="closeDeleteModal"
+                        @confirm-delete="deleteCard()" name="delete-confirmation-modal" />
   </section>
   <section v-else>
     <slot name="noData" />
@@ -189,7 +108,6 @@
 import EditModal from "./Modal/EditModal.vue";
 import DeleteConfirmation from "./Modal/DeleteConfirmation.vue";
 import { ref, defineProps, defineEmits } from "vue";
-
 const isModalOpened = ref(false);
 const isDragging = ref(false);
 const draggedIndex = ref(null);
@@ -199,84 +117,35 @@ const isEdit = ref(false);
 let addingCard = ref(null);
 let editSectionTitle = ref(null);
 let addinglist = ref(null);
-// const slots = useSlots();
-
 const props = defineProps({
-  responseData: {
-    type: Array,
-    required: true,
-  },
-  addCardTitle: {
-    type: String,
-    default: "Add Card",
-  },
-  addSectionTitle: {
-    type: String,
-    default: "Add Section",
-  },
-  isOpenDeleteModal: {
-    type: Boolean,
-    default: false,
-  },
-  isCustomEdit: {
-    type: Boolean,
-    default: false,
-  },
+  responseData: { type: Array, required: true },
+  addCardTitle: { type: String, default: "Add Card" },
+  addSectionTitle: { type: String, default: "Add Section" },
+  isOpenDeleteModal: { type: Boolean, default: false },
+  isCustomEdit: { type: Boolean, default: false },
 });
-const emit = defineEmits([
-  "add-card",
-  "edit-card",
-  "delete-card",
-  "dragstart",
-  "dropItem",
-  "column-added",
-  "edit-section-title",
-]);
-
+const emit = defineEmits(["add-card", "edit-card", "delete-card", "dragstart", "dropItem", "column-added", "edit-section-title"]);
 const response = ref(props.responseData);
 const isOpenDeleteModal = ref(props.isOpenDeleteModal);
 const geteditData = ref({});
 const deleteCardData = ref({});
-const newCard = ref({
-  title: "",
-  description: "",
-});
-const newList = ref({
-  listTitle: "",
-});
+const newCard = ref({ title: "", description: "" });
+const newList = ref({ listTitle: "" });
 const openModal = (event, arrayindex, itemindex, val) => {
   isEdit.value = val;
-  geteditData.value = {
-    event,
-    arrayindex,
-    itemindex,
-  };
+  geteditData.value = { event, arrayindex, itemindex };
   if (props.isCustomEdit == true) {
-    emit("edit-card", {
-      data: geteditData.value,
-      oldData: response.value[arrayindex].data[itemindex],
-      response: response.value,
-    });
+    emit("edit-card", { data: geteditData.value, oldData: response.value[arrayindex].data[itemindex], response: response.value });
     return;
   }
   document.body.classList.add("tvd__modal-open");
   isModalOpened.value = true;
 };
 const openDeleteModal = (arrayindex, itemindex) => {
-  deleteCardData.value = {
-    itemindex,
-    arrayindex,
-  };
+  deleteCardData.value = { itemindex, arrayindex };
   if (props.isCustomEdit === true) {
-    const deletecontent =
-      response.value[deleteCardData.value.arrayindex].data[
-        deleteCardData.value.itemindex
-      ];
-    emit("delete-card", {
-      data: deletecontent,
-      event: deleteCardData.value,
-      response: response.value,
-    });
+    const deletecontent = response.value[deleteCardData.value.arrayindex].data[deleteCardData.value.itemindex];
+    emit("delete-card", { data: deletecontent, event: deleteCardData.value, response: response.value });
     return;
   }
   document.body.classList.add("tvd__modal-open");
@@ -313,11 +182,7 @@ const addCard = (index) => {
       deadlineDate: new Date().toISOString(),
       attachment: [],
     });
-    emit("add-card", {
-      index: index,
-      value: response.value[index].data[response.value[index].data.length - 1],
-      updatedValue: response.value,
-    });
+    emit("add-card", { index: index, value: response.value[index].data[response.value[index].data.length - 1], updatedValue: response.value });
     newCard.value.title = "";
     newCard.value.description = "";
     addingCard.value = null;
@@ -328,10 +193,7 @@ const addList = () => {
     if (!response.value) {
       response.value = { data: [] };
     }
-    response.value.push({
-      title: `${newList.value.listTitle}`,
-      data: [],
-    });
+    response.value.push({ title: `${newList.value.listTitle}`, data: [] });
     emit("column-added", { response: response.value });
     newList.value.listTitle = "";
     addinglist.value = null;
@@ -345,34 +207,17 @@ const editdata = (event) => {
   response.value[arrayIndex].data[itemIndex].description = event.description;
   response.value[arrayIndex].data[itemIndex].deadlineDate = event.deadlineDate;
   response.value[arrayIndex].data[itemIndex].attachment = event.attachment;
-
-  emit("edit-card", {
-    data: event,
-    newData: response.value[arrayIndex].data[itemIndex],
-    oldData: datatoUpdate,
-    response: response.value,
-  });
+  emit("edit-card", { data: event, newData: response.value[arrayIndex].data[itemIndex], oldData: datatoUpdate, response: response.value });
   document.body.classList.remove("tvd__modal-open");
   isModalOpened.value = false;
 };
 const deleteCard = () => {
   document.body.classList.remove("tvd__modal-open");
-  const deletecontent =
-    response.value[deleteCardData.value.arrayindex].data[
-      deleteCardData.value.itemindex
-    ];
-  response.value[deleteCardData.value.arrayindex].data.splice(
-    deleteCardData.value.itemindex,
-    1
-  );
-  emit("delete-card", {
-    data: deletecontent,
-    event: deleteCardData.value,
-    response: response.value,
-  });
+  const deletecontent = response.value[deleteCardData.value.arrayindex].data[deleteCardData.value.itemindex];
+  response.value[deleteCardData.value.arrayindex].data.splice(deleteCardData.value.itemindex, 1);
+  emit("delete-card", { data: deletecontent, event: deleteCardData.value, response: response.value });
   isOpenDeleteModal.value = false;
 };
-
 const dragStart = (arrayIndex, itemIndex) => {
   draggedItem.value = itemIndex;
   draggedArray.value = arrayIndex;
@@ -400,13 +245,10 @@ const internalDrop = (targetArrayIndex, targetIndex) => {
 const dropItem = (targetArrayIndex, event) => {
   event.preventDefault();
   if (draggedItem.value === null || draggedArray.value === null) return;
-
   const targetArray = response.value[targetArrayIndex];
   const targetList = event.target.parentElement;
   if (targetList && targetList.children !== null) {
-    const targetIndex = Array.from(targetList && targetList.children).indexOf(
-      event.target
-    );
+    const targetIndex = Array.from(targetList && targetList.children).indexOf(event.target);
     if (targetIndex === targetList && targetList.children.length - 1) {
       const sourceArrayIndex = draggedArray.value;
       if (sourceArrayIndex !== targetArrayIndex) {
@@ -418,11 +260,7 @@ const dropItem = (targetArrayIndex, event) => {
       const sourceArrayIndex = draggedArray.value;
       if (sourceArrayIndex !== targetArrayIndex) {
         const sourceArray = response.value[sourceArrayIndex];
-        targetArray.data.splice(
-          targetIndex,
-          0,
-          sourceArray.data[draggedItem.value]
-        );
+        targetArray.data.splice(targetIndex, 0, sourceArray.data[draggedItem.value]);
         sourceArray.data.splice(draggedItem.value, 1);
       }
     }
@@ -431,7 +269,6 @@ const dropItem = (targetArrayIndex, event) => {
   draggedItem.value = null;
   draggedArray.value = null;
 };
-
 const showEditCardUI = (index) => {
   if (editSectionTitle.value === null) {
     editSectionTitle.value = index;
